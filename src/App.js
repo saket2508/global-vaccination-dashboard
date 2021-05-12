cf4ef61ec5c23b436a5d9edd6d7e2f9afe14d813
saket2508 <saketns@gmail.com>
2021-05-12 18:02:33 +0530

    map interface ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
 function App() {
+
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 40.7128,
+    longitude: -74.0060,
+    zoom: 2
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="header">
+      <div className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <h6 className="mx-auto navbar-brand">
+            Global Vaccination Tracker
+          </h6>
+        </div>
+      </div>
     </div>
+    <div className="map">
+       <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/saket2000/ckolf18ga1lxq17l31rw3lrxk"
+      > 
+      </ReactMapGL>
+    </div>
+    </>
   );
 }
 
